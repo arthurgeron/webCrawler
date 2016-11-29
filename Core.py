ea27441cb751c75c9e45b9c509a35b5e384151f3
Arthur Geron <arthurgeron@hotmail.com>
2016-11-29 04:00:59 -0200

    Script now displays total of URLs analysed Changed function name to something more precise Added more comments to the code

--- Core.py
+++ Core.py
@@ -10,20 +10,23 @@ dupcheck = set()
 queue = None  # Html Queue
 textToLookFor = None
 searchingLimit = None
+matchTextRegex = None
 matchesList = []
 
 
-def processInitialUserInput():
-    global queue, textToLookFor, searchingLimit
+def processInitialUserInputAndInitiateVariables():
+    global queue, textToLookFor, searchingLimit, matchTextRegex
     searchingLimit = raw_input("Type in the maximum number of pages you want to search into:\n")
-    queue = Queue.Queue(int(searchingLimit))
-    if len(sys.argv) < 2:
+    queue = Queue.Queue(int(searchingLimit))  # It's best to hard-code the queue's limit just in case
+    if len(sys.argv) < 2:   # This was an experimental flag, which allows the user to run the python script with flags
         queue.put('http://' + raw_input("Type in a link without http//: \n"))
     else:
         queue.put(sys.argv[1])
     textToLookFor = raw_input("Type in a word that you want to look for: \n")
     logFile = open("errorLog.txt", "w")
     logFile.close()
+    matchTextRegex = [r"((<p([A-Za-z-0-9=\"'_ \\-]|\s){0,}?[>])){1}(.{0,}?",
+                      r".{0,}?)(<[\/|\\]?([A-Za-z-0-9=\"'_ \-]|\s){0,}?p[>]){1}"]
 
 
 def processAndTellResult():
@@ -35,16 +38,18 @@ def processAndTellResult():
     for match in matchesList:
         resultFiles.write(match)
     resultFiles.close()
-    print("Done! Matches have been written into matches.txt \n")
+    print("Done! Matches have been written into matches.txt \nURLs processed: " + str(processedURLsCounter))
 
 
 def queueURLs(html, origLink):  # Processes HTML code looking for other URLs inside the domain
-    for url in re.findall('''<a[^>]+href=["'](.[^"']+)["']''', html, re.I):
-        link = url.split("#", 1)[0] if url.startswith("http") else '{uri.scheme}://{uri.netloc}'.format(uri=urlparse.urlparse(origLink)) + url.split("#", 1)[0]
+    for url in re.findall('''<a[^>]+href=["'](.[^"']+)["']''', html, re.I):  # Searches the HTML for other URLs
+        link = url.split("#", 1)[0] \
+            if url.startswith("http") \
+            else '{uri.scheme}://{uri.netloc}'.format(uri=urlparse.urlparse(origLink)) + url.split("#", 1)[0]
         if link in dupcheck:  # Checks if link has already been processed
             continue
         dupcheck.add(link)
-        if len(dupcheck) > 99999:
+        if len(dupcheck) > 99999:  # It's better to avoid memory overflow problems
             dupcheck.clear()
         if processedURLsCounter >= int(searchingLimit):
             with queue.mutex:  # Clears queue in a thread safe way
@@ -56,30 +61,34 @@ def queueURLs(html, origLink):  # Processes HTML code looking for other URLs ins
 def getHTML(link):
     try:
         global processedURLsCounter
-        processedURLsCounter = processedURLsCounter + 1
-        html = urllib.urlopen(link).read()
+        processedURLsCounter += 1
+        html = urllib.urlopen(link).read()  # Here we transform a URL into a string containing the HTML code of the page
         print("Processing link: " + link + "\n")
-        for match in re.findall(r"((<p([A-Za-z-0-9=\"'_ \\-]|\s){0,}?[>])){1}(.{0,}?" + textToLookFor + r".{0,}?)(<[\/|\\]?([A-Za-z-0-9=\"'_ \-]|\s){0,}?p[>]){1}", html):
+        # Bellow it will scan the HTML code for texts inside P elements which contain the chosen word or phrase
+        for match in re.findall(matchTextRegex[0] + textToLookFor + matchTextRegex[1], html, re.I):
+            # If it gets inside the loop it has basically found a match
             print("Match found:\n")
-            print(str(match[3]) + "\n\n")
+            print(str(match[3]) + "\n\n")  # Warns the user that it has found a match
+            # Adds match to list so we can throw all that into a text file later, doing this while processing HTMLs
+            # will just slow the process
             matchesList.append("Link:\n " + link + "\nMatched paragraph:\n " + match[3] + "\n\n")
-        queueURLs(html, link)  # Asks queueURLs to look for other links in that page
-    except (KeyboardInterrupt, SystemExit):
+        queueURLs(html, link)  # Query queueURLs to look for other links in that page
+    except (KeyboardInterrupt, SystemExit):  # User can interrupt the process anytime
         dupcheck.add(link)
         raise
     except Exception, e:
-        dupcheck.add(link)
+        dupcheck.add(link)  # If the link generates an exception we need to make sure it won't process it again
         print("Error while processing: " + link + " check errorLog.txt for more details \n")
         logFile = open("errorLog.txt", "a")
-        logFile.write("Error processing " + link + " : " + str(e) + "\n")
+        logFile.write("Error processing " + link + " : " + str(e) + "\n")  # Writes error to log file
         logFile.close()
         pass
 
 
-processInitialUserInput()
+processInitialUserInputAndInitiateVariables()
 
-while queue.empty() is not True or len(dupcheck) < 1:
+while queue.empty() is not True or len(dupcheck) < 1:  # Core loop of this script, starts the chain effect
     thread.start_new_thread(getHTML, (queue.get(),))
-    time.sleep(0.5)
+    time.sleep(0.5)  # Without this the while loop will cycle too fast and think that the queue is empty
 
 processAndTellResult()
